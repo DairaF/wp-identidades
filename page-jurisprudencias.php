3cba92c5b50ed6f47f4b68b0c7054377f3816083
chechubarco <cecibarco@gmail.com>
2022-05-08 13:08:49 -0300

    Update jurisprudencia (remove _blank)

--- page-jurisprudencias.php
+++ page-jurisprudencias.php
@@ -79,7 +79,7 @@
           while ( $loop->have_posts() && $ok == true ) : $loop->the_post(); if ($search == null || str_contains($minTitle,$minSearch)): ?>
     <!-- CARD -->
     <div class="">
-          <a href="<?php the_permalink() ?> " target="blank" class="card" 
+          <a href="<?php the_permalink() ?> "  class="card" 
             style="background-image:url('<?php the_field('foto') ?>'); background-repeat:no-repeat; background-size:cover; background-position:center center;">
             <div class="card-body">
               <h4 class="card-title mb-3 d-block"><?php the_title() ?></h4>
